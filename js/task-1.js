9af9ff028a817b5b7e405823a8a0f121fc6ffddd
Mari K. <marichkakanivets7@gmail.com>
2026-08-01 13:49:26 -1200

    first commit

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+function isEnoughCapacity(products, containersize) {
+    let totalProducts = 0;
+    for (const quantity of Object.values(products)) {
+        totalProducts += quantity;
+    }
+    return totalProducts <= containersize;
+}
---
+++ js/task-2.js
@@ -0,0 +1,13 @@
+function calcAverageCalories(days) {
+    if (days.length === 0) {
+        return 0;
+    }
+
+    let totalCalories = 0;
+
+    for (const day of days) {
+        totalCalories += day.calories;
+    }
+    
+    return totalCalories / days.length;
+}
